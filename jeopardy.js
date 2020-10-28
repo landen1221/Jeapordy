2fe922b0a6e04f8a3f28d2ebb6bf8c211e0eaba9
Matthew Landen <mlanden1221@gmail.com>
2020-10-28 12:49:41 -0700

    Pushing changes post mentor call

--- jeopardy.js
+++ jeopardy.js
@@ -1,21 +1,25 @@
 let categories = [];
 let initialID = 11531;
 let gameCategories = []
+const initialHTML = document.querySelector('#tableDiv').innerHTML
 
 // get list of 100 categories
 async function getCategoryIds(initialID) {
-    const res = await axios.get('https://jservice.io/api/categories', {params: {count: 100, offset: initialID}})
-    for (let i =0; i< res.data.length; i++) {
-        categories.push(res.data[i].id)
+    const {data} = await axios.get('https://jservice.io/api/categories', {params: {count: 100, offset: initialID}})
+    
+    for (let i =0; i< data.length; i++) {
+        categories.push(data[i].id)
     }
-    return res.data[res.data.length-1].id
+
+    return data[data.length-1].id
 }
 
 // Adds 300 possible categories to categories list
-async function addThreeHundredCategories() {
+async function addThreeHundredCategories() {  
     let x = await getCategoryIds(initialID)
     let y = await getCategoryIds(x)
     await getCategoryIds(y)
+
     getGameCategories()  
 }
 
@@ -33,7 +37,6 @@ function getGameCategories() {
 
 // set header row with category title
 async function setHeaderRow(gameCat) {
-    
     for (let i=0; i< gameCat.length; i++) {
         // get game category
         let x = gameCat[i]
@@ -47,33 +50,46 @@ async function setHeaderRow(gameCat) {
 }
 
  // Functionality to show question or solution when cell is clicked
- $(document).click(async function(event) {
+ let tableTDs = document.querySelectorAll('td')
+ $(tableTDs).click(async function(event) {
     let IDName = $(event.target).get()[0].id;
     
-    if (IDName.slice(0,4) === 'cell') {
-        let category = IDName.slice(7,8)  // column
-        let clue = IDName.slice(5,6) // row
-        
-        let res = await axios.get('https://jservice.io/api/category', { params: {id: gameCategories[category]}})
-        
-        let cell = document.getElementById(IDName)
-        
-        cell.style.lineHeight = '1em'
-        cell.style.verticalAlign = 'middle'
-        
-        if (cell.className === 'visibleQuestion') {    
-            cell.style.fontSize = '1.25em'
-            cell.innerHTML = res.data.clues[clue].answer
-            cell.classList.toggle('visibleQuestion')
-        } else {
-            cell.style.fontSize = '1em'
-            cell.innerHTML = res.data.clues[clue].question
-            cell.classList.add('visibleQuestion')
-        }      
-    }
+    let category = IDName.slice(7,8)  // column
+    let clue = IDName.slice(5,6) // row
+    
+    let res = await axios.get('https://jservice.io/api/category', { params: {id: gameCategories[category]}})
+    
+    let cell = document.getElementById(IDName)
+    
+    cell.style.lineHeight = '1em'
+    cell.style.verticalAlign = 'middle'
+    
+    if (cell.className === 'visibleQuestion') {    
+        cell.style.fontSize = '1.25em'
+        cell.innerHTML = res.data.clues[clue].answer
+        cell.classList.toggle('visibleQuestion')
+    } else {
+        cell.style.fontSize = '1em'
+        cell.innerHTML = res.data.clues[clue].question
+        cell.classList.add('visibleQuestion')
+    }      
+    
 });
 
-// Start game:
+// Start game functionality:
+async function loadPage() {
+    // debugger
+    await addThreeHundredCategories()
+    await setHeaderRow(gameCategories)
+    
+    document.getElementById('table').style.visibility = "visible"
+
+    let button = document.querySelector('button')
+    button.style.backgroundColor = 'red'
+    button.innerText = 'Restart'
+}
+
+// Handle button functionality
 const btn = document.querySelector('button')
 btn.addEventListener('click', function(e) {
     e.preventDefault()
@@ -86,72 +102,3 @@ btn.addEventListener('click', function(e) {
         loadPage()
     }
 })
-
-async function loadPage() {
-
-    await addThreeHundredCategories()
-    await setHeaderRow(gameCategories)
-    
-    document.getElementById('table').style.visibility = "visible"
-
-    let button = document.querySelector('button')
-    button.style.backgroundColor = 'red'
-    button.innerText = 'Restart'
-}
-
-const initialHTML = `<table id="table" class="table table-bordered text-center ">
-<thead>
-      <tr>
-            <th scope="col" id="header-0" class="headers">""</th>
-            <th scope="col" id="header-1" class="headers">""</th>
-            <th scope="col" id="header-2" class="headers">""</th>
-            <th scope="col" id="header-3" class="headers">""</th>
-            <th scope="col" id="header-4" class="headers">""</th>
-            <th scope="col" id="header-5" class="headers">""</th>
-      </tr>
-</thead>
-<tbody>
-      <tr class="h-25">
-            <td id="cell-0.0">?</td>
-            <td id="cell-0.1">?</td>
-            <td id="cell-0.2">?</td>
-            <td id="cell-0.3">?</td>
-            <td id="cell-0.4">?</td>
-            <td id="cell-0.5">?</td>
-      </tr>
-      <tr>
-            <td id="cell-1.0">?</td>
-            <td id="cell-1.1">?</td>
-            <td id="cell-1.2">?</td>
-            <td id="cell-1.3">?</td>
-            <td id="cell-1.4">?</td>
-            <td id="cell-1.5">?</td>
-      </tr>
-      <tr>
-            <td id="cell-2.0">?</td>
-            <td id="cell-2.1">?</td>
-            <td id="cell-2.2">?</td>
-            <td id="cell-2.3">?</td>
-            <td id="cell-2.4">?</td>
-            <td id="cell-2.5">?</td>
-      </tr>
-      <tr>
-            <td id="cell-3.0">?</td>
-            <td id="cell-3.1">?</td>
-            <td id="cell-3.2">?</td>
-            <td id="cell-3.3">?</td>
-            <td id="cell-3.4">?</td>
-            <td id="cell-3.5">?</td>
-      </tr>
-      <tr>
-            <td id="cell-4.0">?</td>
-            <td id="cell-4.1">?</td>
-            <td id="cell-4.2">?</td>
-            <td id="cell-4.3">?</td>
-            <td id="cell-4.4">?</td>
-            <td id="cell-4.5">?</td>
-      </tr>
-      
-</tbody>
-</table>
-</div>`
